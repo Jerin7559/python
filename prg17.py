20f0c57ecfdb7e8b123be1de3838fc37e0ade0f9
Jerin7559 <109576021+Jerin7559@users.noreply.github.com>
2023-01-12 11:46:39 +0530

    Delete prg17.py

--- prg17.py
+++
@@ -1,8 +0,0 @@
-n=int(input("enter the step size:"))
-print("the pyramid for the given size",n,"is:")
-for i in range(1,n+1):
-    k = i
-    for j in range(1,i+1):
-        print( k,end="")
-        k+=i
-    print("")
