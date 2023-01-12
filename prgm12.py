8387ee2b6c6c514f2b594c7c5aab9bb96087aa88
Jerin7559 <109576021+Jerin7559@users.noreply.github.com>
2023-01-12 11:53:10 +0530

    Delete prgm12.py

--- prgm12.py
+++
@@ -1,8 +0,0 @@
-def change_char(str1):
-    char = str1[0]
-    str1 = str1.replace(char,'$')
-    str1 = char+str1[1:]
-    return str1
-str = input("enter the string:")
-print("the original string:",str)
-print("the replaced string:",change_char(str))
