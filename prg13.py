ab94f75b097a4416011d9b8decb77825f58a8f7a
Jerin7559 <109576021+Jerin7559@users.noreply.github.com>
2023-01-12 11:44:30 +0530

    Delete prg13.py

--- prg13.py
+++
@@ -1,7 +0,0 @@
-def character_exchange(str1):
-    str2 = (str1[-1:] + str1[1:+-1] + str1[:1])
-    return str2
-str = input("enter the string:")
-print("The original sring:", str)
-print("string after swappinng first and last character:",character_exchange(str))
-
